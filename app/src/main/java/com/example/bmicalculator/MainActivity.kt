d7c0749ef56257afe65febb01b6b750e26101c71
DELL <anushkabarai@gmail.com>
2023-10-10 02:18:19 +0530

    Final Commit

---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.bmicalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bmicalculator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity()
+{
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.weightPicker.minValue = 30
+        binding.weightPicker.maxValue = 150
+
+
+        binding.heightPicker.minValue = 90
+        binding.heightPicker.maxValue = 250
+
+        binding.weightPicker.setOnValueChangedListener{ _,_,_->
+            calculateBMI()
+        }
+        binding.heightPicker.setOnValueChangedListener{ _,_,_->
+            calculateBMI()
+        }
+
+
+    }
+
+    private fun calculateBMI()
+    {
+        val height = binding.heightPicker.value
+        val doubleheight = height.toDouble()/100
+
+        val weight = binding.weightPicker.value
+
+        val bmi = weight.toDouble()/(doubleheight * doubleheight)
+
+        binding.resultsTV.text = String.format("BMI is : %.2f",bmi)
+        binding.healthyTV.text = String.format("Considered : %s",message(bmi))
+    }
+
+    private fun message(bmi: Double):String
+    {
+        if(bmi < 18.5)
+            return "Underweight"
+        if(bmi < 25.0)
+            return "Healthy"
+        if(bmi < 30.0)
+            return "Overweight"
+
+        return "Obese"
+    }
+
+
+}
